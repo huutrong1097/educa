78d70c18c2b9906dd6902d2a32ce017841f2d413
TMA\nhuutrong <nhuutrong@tma.com.vn>
2021-06-17 11:29:21 +0700

    Ađ comment to urls file

--- educa/urls.py
+++ educa/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
+#Hello my friend
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name ='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name = 'logout'),
